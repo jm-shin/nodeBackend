cfb63f26f7b71048ce3abcb87a021eedfa367b90
jongmin <ace4463@gmail.com>
2021-02-04 15:11:58 +0900

    Header 생성

---
+++ client/src/components/Base/HeaderContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+//import Header from 'components/Base/Header/Header';
+import Header from './Header/Header';
+
+class HeaderContainer extends Component {
+  render() {
+    return <Header>Hi</Header>;
+  }
+}
+
+export default HeaderContainer;
---
+++ client/src/containers/Base/HeaderContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// import Header from 'components/Base/Header';
+import Header from '../../components/Base/Header/Header';
+
+class HeaderContainer extends Component {
+  render() {
+    return <Header>Hi</Header>;
+  }
+}
+
+export default HeaderContainer;
