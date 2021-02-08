be1102cde47dce7ccb139f7b50df5f29b3157056
jongmin <ace4463@gmail.com>
2021-02-08 13:06:08 +0900

    로그인 연동

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,28 @@ import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import configureStore from './redux/configureStore';
+import { AppContainer } from 'react-hot-loader';
+import socket from './lib/socket';
 
 const store = configureStore();
 
+// 개발환경에선 localhost:4000 에 연결하고, 프로덕션에선 현재 호스트에 알맞는 프로토콜로 접속합니다
+const socketURI =
+  process.env.NODE_ENV === 'production'
+    ? (window.location.protocol === 'https:' ? 'wss://' : 'ws://') +
+      window.location.host +
+      '/ws'
+    : 'ws://localhost:4000/ws';
+
+socket.initialize(store, socketURI);
+
 const render = Component => {
-  ReactDOM.render(<Component store={store} />, document.getElementById('root'));
+  ReactDOM.render(
+    <AppContainer>
+      <Component store={store} />
+    </AppContainer>,
+    document.getElementById('root')
+  );
 };
 
 render(Root);
--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -1,11 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import penderMiddleware from 'redux-pender';
 import modules from './modules';
+
+const isDevelopment = process.env.NODE_ENV === 'development'; // 환경이 개발모드인지 확인합니다
+const composeEnhancers = isDevelopment
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  : compose;
 
 // Todo: 미들웨어 적용
 const configureStore = initialState => {
   const store = createStore(
     modules,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    initialState,
+    composeEnhancers(applyMiddleware(penderMiddleware()))
   );
 
   // hot-reloading 를 위한 코드
